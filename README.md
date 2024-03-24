8bf5c65975bd8e44d17b774adcaae4843fd32820
Hexord <jhonymcb123@gmail.com>
2024-03-23 23:11:39 -0600

    Completed script with validations

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # odin-rockpaperscissors
+
+This exercise from The Odin Project is to prove my knowledge I adquired on the previous lessons of Javascript in Foundations course building a Rock Paper Scissors game that can be played on the Web console.
---
+++ script.js
@@ -0,0 +1,93 @@
+let playerCounter = 0;
+let computerCounter = 0;
+let gameCounter = 0;
+
+function getComputerChoice() {
+    let ComputerChoice = Math.floor(Math.random() * 3) + 1;
+    if (ComputerChoice === 1) {
+        return "Rock"
+    }
+    if (ComputerChoice === 2) {
+        return "Paper"
+    } else {
+        return "Scissors"
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    let upperLetter = playerSelection.charAt(0).toUpperCase();
+    playerSelection = upperLetter + playerSelection.toLowerCase().substring(1);
+
+
+    if( !playerSelection.includes("Rock") && !playerSelection.includes("Paper") && !playerSelection.includes("Scissors")){
+        return "Please write a valid option"
+    }
+    
+    if (playerSelection === computerSelection){
+        return `It's a tie! ${playerSelection} doesn't beat ${computerSelection}`
+    } else {
+        if(playerSelection === "Rock"){
+            if(computerSelection === "Paper"){
+                return `You Lose! ${computerSelection} beats ${playerSelection}`
+            } else {
+                return `You Won! ${playerSelection} beats ${computerSelection}`
+            }
+        } else if(playerSelection === "Paper"){
+            if(computerSelection === "Scissors"){
+                return `You Lose! ${computerSelection} beats ${playerSelection}`
+            } else {
+                return `You Won! ${playerSelection} beats ${computerSelection}`
+            }
+        } else if(playerSelection === "Scissors"){
+            if(computerSelection === "Rock"){
+                return `You Lose! ${computerSelection} beats ${playerSelection}`
+            } else {
+                return `You Won! ${playerSelection} beats ${computerSelection}`
+            }
+        }  else {
+            return "Please write a valid option"
+        }
+    }    
+}
+
+function playGame(){
+    let result;
+    do {
+        let playerChoice = prompt("Please select one of the three options: Rock, Paper or Scissors");
+
+        let computerSelection = getComputerChoice();
+    
+        result = playRound(playerChoice, computerSelection)
+        
+        if(result.includes('Won')){
+            playerCounter++
+        } else if(result.includes('Lose')){
+            computerCounter++
+        } else {
+            alert(result);
+        }
+    } while(result.includes('Please write a valid option'))
+
+    gameCounter++
+
+    console.log(gameCounter + ': ' + result)
+    
+    if(gameCounter === 5){
+        console.log(`player : ${playerCounter}, computer: ${computerCounter}`)
+        if(playerCounter === computerCounter){
+            console.log('Tie in the match!!!')
+        }
+        if(playerCounter > computerCounter){
+            console.log('You lon the Match!!!')
+        }
+        if(playerCounter < computerCounter){
+            console.log('You lose the Match :C')
+        }
+    }
+}
+
+playGame()
+playGame()
+playGame()
+playGame()
+playGame()
